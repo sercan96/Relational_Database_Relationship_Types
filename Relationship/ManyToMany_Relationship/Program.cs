25f5ca4a0289227fbf82c86c977361832d342011
sercan96 <38535473+sercan96@users.noreply.github.com>
2024-02-11 02:10:09 +0300

    Many To Many Relationship Added

---
+++ Relationship/ManyToMany_Relationship/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManyToMany_Relationship
+{
+    /* 
+     * 1- Add Migration --- 
+     * 2- Update Database
+    */
+    #region Default Convension
+
+    /* 
+     * İki entity arasındaki ilişkiyi navigation propertyler üzerinden çoğul olarak kurmalıyız.
+     * (ICollection,List)
+     * Default Convention'da cross table'ı(ara tablo) manuel oluşturmak zorunda değiliz. EF Core tasarıma uygun şekilde cross table'i kendisi otomatik basacak ve genereta edecektir.
+     * Ve oluşturulan cross table'ın içerisinde composite primary key'i(ikili unik anahtar) otomatik oluşturmuş olacaktır.
+    */
+    //public class Book // Dependent Entity(Book)
+    //{
+    //    public int Id { get; set; }
+    //    public string BookName { get; set; }
+    //    public List<Author> Authors { get; set; }
+
+    //}
+
+    //public class Author
+    //{
+    //    public int Id { get; set; }
+    //    public string AuthorName { get; set; }
+    //    public List<Book> Books { get; set;}
+    //}
+
+
+
+    #endregion
+
+    #region Data Annotation
+
+    //Cross Table(Ara Tablo-BookAuthor) manuel olarak oluşturulmak zorundadır.
+    // Entity'lerde oluşturduğumuz cross table entity si ile bire çok ilişki kurulmalı.
+    // CT'da composite primary key'i data annotation(attributes) lar ile manuel kuramıyoruz. Bunun için Fluent Api ile çalışma yapmamız gerekiyor.
+    // Cross table'a karşılık bir entity modeli oluşturuyorsak eğer bunu context sınıfı içerisinde DbSet propertysi olarak bildirmek mecburiyetinden değiliz.
+
+    //public class Book // Dependent Entity(Book)  (Kitap ile KitapYazar arasında -- OneToMany)
+    //{
+    //    public int Id { get; set; }
+    //    public string BookName { get; set; }
+    //    public List<BookAuthor> Authors { get; set; }
+
+    //}
+
+    //public class BookAuthor 
+    //{
+    //    public int BookId { get; set; }
+    //    public int AuthorId { get; set; }
+    //    public Book Book { get; set; }
+    //    public Author Author { get; set; }
+
+    //}
+
+    //public class Author //(Yazar ile KitapYazar arasında -- OneToMany)
+    //{
+    //    public int Id { get; set; }
+    //    public string AuthorName { get; set; }
+
+    }
+    #endregion
+
+    #region Fluent Api
+
+    /*
+     * Cross Table manuel olarak oluşturmak zorunda
+     * DBSet olarak eklenmesine gerek yok
+     * Composite PK HasKey metodu ile kurulmalı
+    */
+    public class Book // Dependent Entity(Book)  (Kitap ile KitapYazar arasında -- OneToMany)
+    {
+        public int Id { get; set; }
+        public string BookName { get; set; }
+        public List<BookAuthor> Authors { get; set; }
+
+    }
+
+    public class BookAuthor
+    {
+        public int BookId { get; set; }
+        public int AuthorId { get; set; }
+        public Book Book { get; set; }
+        public Author Author { get; set; }
+
+    }
+
+    public class Author //(Yazar ile KitapYazar arasında -- OneToMany)
+    {
+        public int Id { get; set; }
+        public string AuthorName { get; set; }
+
+    }
+    #endregion
+
+    public class ECommerceDbContext : DbContext
+    {
+        public ECommerceDbContext() : base("MyConnection")
+        { }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        //Data Anotation
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<BookAuthor>()
+        //                .HasKey(ba => new
+        //                {
+        //                    ba.BookId,
+        //                    ba.AuthorId
+        //                });
+        //}
+
+        // Fluent Api
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<BookAuthor>()
+        //               .HasKey(ba => new { ba.BookId, ba.AuthorId });
+
+        //    modelBuilder.Entity<BookAuthor>()
+        //                   .HasOne(ba => ba.Book)
+        //                   .HasMany(b => Authors)
+        //                   .HasForeignKey(ba => ba.BookId);
+
+        //    modelBuilder.Entity<BookAuthor>()
+        //                    .HasOne(ba => ba.Author)
+        //                    .HasMany(a => Books)
+        //                    .HasForeignKey(ba => ba.AuthorId);
+        //}
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- Relationship/OneToOne_Relationship/Program.cs
+++ Relationship/OneToOne_Relationship/Program.cs
@@ -5,12 +5,43 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Linq;
 using System.Runtime.Remoting.Contexts;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using static OneToOne_Relationship.Program;
 
 namespace OneToOne_Relationship
 {
+    #region Default Convension
+    /*
+     * Her iki entity'de Navigation property ile birbirlerini tekil olarak referans ederek fiziksel bir ilişki olcağını beyan eder.
+     * One to One ilişki türünde, dependent entity nin hangisi olduğunu default olarak belirleyebilmek kolay değildir.Bu durumda fiziksel olarak bir foreign keye karşılık bir colon tanımlayarak çözüm getirebiliyoruz.
+     * Böylece foreign key e karşılık property ekleyerek lüzümsuz bir kolon eklemiş oluyoruz.
+    */
+    //public class Employee // Principal
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //    public string Surname { get; set; }
+    //    public EmployeeAdress EmployeeAdress { get; set; }
+    //}
+    //public class EmployeeAdress // Dependent(Bağımlı) => Bağımlı olana foreign key eklenir. 
+    //{
+    //    public int Id { get; set; }
+    //    public int EmployeeID { get; set; }
+    //    public string Adress { get; set; }
+    //    public Employee Employee { get; set; }
+
+    //}
+    #endregion
+    #region Data Annotation
+
+    /*
+     * Navigation Propertyler tanımlanmalıdır.
+     * Foreign kolonunun ismi default converntionun dışında bir kolon olucaksa eğer Foreign attribute ile bunu bildirebilirsiniz.
+     * Foreign kolonu oluşturulmak zorunda değildir.
+     * 1'e 1 ilişkide ekstradan foreign key kolonuna ihtiyaç olmayacağından dolayı dependent entity deki Id kolonunun hem primary key hem de foreign key olarak kullanmayı tercih ediyıruz ve bu duruma özen gösterilmelidir diyoruz.
+    */
     public class Employee // Principal
     {
         public int Id { get; set; }
@@ -20,12 +51,32 @@ namespace OneToOne_Relationship
     }
     public class EmployeeAdress // Dependent(Bağımlı) => Bağımlı olana foreign key eklenir. 
     {
-        [Key,ForeignKey(nameof(Employee))]
+        [Key, ForeignKey(nameof(Employee))]
         public int Id { get; set; }
         public string Adress { get; set; }
         public Employee Employee { get; set; }
 
     }
+    #endregion
+    #region Fluent Api
+
+    // Entity ler üzerinden çalışmıyoruz. İlişkiyi OnModelCreating metodunda yapıyrouz.
+    //public class Employee // Principal
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //    public string Surname { get; set; }
+    //    public EmployeeAdress EmployeeAdress { get; set; }
+    //}
+    //public class EmployeeAdress // Dependent(Bağımlı) => Bağımlı olana foreign key eklenir. 
+    //{        
+    //    public int Id { get; set; }
+    //    public string Adress { get; set; }
+    //    public Employee Employee { get; set; }
+
+    //}
+    #endregion
+
     public class ECommerceDbContext : DbContext
     {
         public ECommerceDbContext() : base("MyConnection")
@@ -35,7 +86,13 @@ namespace OneToOne_Relationship
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Employee>().HasOptional(x => x.EmployeeAdress).WithRequired(x => x.Employee);
+            //modelBuilder.Entity<EmployeeAdress>()
+            //            .HasKey(e => e.Id);
+
+            //modelBuilder.Entity<Employee>()
+            //            .HasOne(x => x.EmployeeAdress)
+            //            .WithOne(ea => ea.Employee)
+            //            .HasForeignKey(x => x.Id);
         }
 
     }
